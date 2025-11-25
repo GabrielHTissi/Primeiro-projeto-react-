eb803563a3873658152bcf845c3041ff2b9f2319
GabrielHTissi <gabriel.hoeft.tissi@icloud.com>
2025-11-25 18:55:05 -0300

    Primeiro comite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Meu Primeiro Carro </h1>
+      <p>Tudo que você precisa saber para comprar o seu primeiro carro!</p>
+    </div>
+  )
+}
+
+export default App
+
